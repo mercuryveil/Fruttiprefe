d3d9799c1360d390e9851daa53659bf90bc06682
Marco <marco.galetta@hotmail.com>
2022-04-03 17:00:27 +0200

    Aggiungere i file di progetto.

---
+++ Fruttiprefe/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FruttiCsharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            String[] frutti = { "Pera", "Mela", "Banana", "Melograno", "Arancia" };
+            ShowArrayInfo(frutti);
+        }
+        private static void ShowArrayInfo(Array arr)
+        {
+            Console.WriteLine("Lunghezza classifica dei miei frutti: {0,4}", arr.Length);
+
+            Console.WriteLine("I miei frutti preferiti in ordine (dal migliore al peggiore) sono: Melograno, Pera, Mela, Banana, Arancia");
+
+            Console.WriteLine("Il mio frutto preferito è: Melograno");
+
+            Console.WriteLine("Il mio ultimo frutto preferito è: Arancia");
+
+        }
+    }
+}
+
